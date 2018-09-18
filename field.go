606b5cf0725289831e2a7893ac7d42de68887e9d
Kevin Seidel <kevin@zahlz.com>
2018-09-18 14:17:43 +0200

    Add Int64()

--- field.go
+++ field.go
@@ -50,6 +50,19 @@ func (f *Field) Int() (int, error) {
 	return strconv.Atoi(f.value)
 }
 
+// Int64 returns the fields value as int64
+// If it is the wrong type or empty an error is returned
+func (f *Field) Int64() (int64, error) {
+	if f.IsEmpty() {
+		return 0, ErrEmptyField
+	}
+	if f.column.Type != TypeNumber {
+		return 0, fmt.Errorf("invalid field type")
+	}
+
+	return strconv.ParseInt(f.value, 10, 64)
+}
+
 // Date returns the fields value as time.Time
 // If it is the wrong type or empty an error is returned
 func (f *Field) Date() (time.Time, error) {
